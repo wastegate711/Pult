d42bd0a6fa87efa1f38be01d05f0d40cee53439f
wastegate711 <evgeni.shvec@gmail.com>
2022-06-05 12:25:17 +0300

    Добавил команды управления подсветкой кнопок.

--- Services/Inc/GlobalSettings.h
+++ Services/Inc/GlobalSettings.h
@@ -11,8 +11,16 @@
 // Настройки.
 #define RECEIV_LEN   255 //максимальная длина принимаемой посылки.
 
-// Команды запросов
-#define GET_STATUS 0x01
-#define GET_UID 0x02
+// Команды
+#define GET_STATUS                      0x01
+#define GET_UID                         0x02
+#define SET_BACKLIGHT_BUTTON_INSECT     0x03
+#define SET_BACKLIGHT_BUTTON_FOAM       0x04
+#define SET_BACKLIGHT_BUTTON_FOAM_WATER 0x05
+#define SET_BACKLIGHT_BUTTON_HOT_WATER  0x06
+#define SET_BACKLIGHT_BUTTON_COOL_WATER 0x07
+#define SET_BACKLIGHT_BUTTON_VOSK       0x08
+#define SET_BACKLIGHT_BUTTON_OSMOS      0x09
+#define SET_BACKLIGHT_BUTTON_STOP       0x0A
 
 #endif //PULT_GLOBALSETTINGS_H
