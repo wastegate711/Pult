f5251bb044c998e1494cb2994b751d3dfd3451b6
wastegate711 <evgeni.shvec@gmail.com>
2022-06-13 14:07:52 +0300

    изменил коды команд.

--- Services/Inc/GlobalSettings.h
+++ Services/Inc/GlobalSettings.h
@@ -15,26 +15,26 @@
 // Команды
 #define GET_STATUS                      0x01 // запрос текущего состояния
 #define GET_UID                         0x02 // запрос серийного номера устройства
-#define SET_BACKLIGHT_BUTTON_INSECT     0x03 // включение/выключение подсветки кнопки Средство от насекомых
-#define SET_BACKLIGHT_BUTTON_FOAM       0x04 // включение/выключение подсветки кнопки Пена
-#define SET_BACKLIGHT_BUTTON_FOAM_WATER 0x05 // включение/выключение подсветки кнопки Пена + вода
-#define SET_BACKLIGHT_BUTTON_HOT_WATER  0x06 // включение/выключение подсветки кнопки Горячая вода
-#define SET_BACKLIGHT_BUTTON_COOL_WATER 0x07 // включение/выключение подсветки кнопки Холодная вода
-#define SET_BACKLIGHT_BUTTON_VOSK       0x08 // включение/выключение подсветки кнопки Воск
-#define SET_BACKLIGHT_BUTTON_OSMOS      0x09 // включение/выключение подсветки кнопки Осмос
-#define SET_BACKLIGHT_BUTTON_STOP       0x0A // включение/выключение подсветки кнопки Стоп
-#define PUSH_BUTTON_INSECT              0x0B // Нажата кнопка Средство от насекомых
-#define PUSH_BUTTON_COLLECTION          0x0C // Нажата кнопка Инкассация
-#define SET_DISPLAY_NUMBER              0x0D // Установка значения на дисплее
-#define PUSH_JETTON_CHANNEL_1           0x0E // Принят жетон по первому каналу
-#define PUSH_JETTON_CHANNEL_2           0x0F // Принят жетон по 2 каналу
-#define PUSH_JETTON_CHANNEL_3           0x10 // Принят жетон по 3 каналу
-#define PUSH_BUTTON_FOAM                0x11 // Нажата кнопка Пена
-#define PUSH_BUTTON_FOAM_WATER          0x12 // Нажата кнопка Пена + вода
-#define PUSH_BUTTON_HOT_WATER           0x13 // Нажата кнопка Горячая вода
-#define PUSH_BUTTON_COOL_WATER          0x14 // Нажата кнопка Холодная вода
-#define PUSH_BUTTON_VOSK                0x15 // Нажата кнопка Воск
-#define PUSH_BUTTON_OSMOS               0x16 // Нажата кнопка Осмос
-#define PUSH_BUTTON_STOP                0x17 // Нажата кнопка Стоп
+#define SET_BACKLIGHT_BUTTON_INSECT     0x16 // включение/выключение подсветки кнопки Средство от насекомых
+#define SET_BACKLIGHT_BUTTON_FOAM       0x17 // включение/выключение подсветки кнопки Пена
+#define SET_BACKLIGHT_BUTTON_FOAM_WATER 0x18 // включение/выключение подсветки кнопки Пена + вода
+#define SET_BACKLIGHT_BUTTON_HOT_WATER  0x19 // включение/выключение подсветки кнопки Горячая вода
+#define SET_BACKLIGHT_BUTTON_COOL_WATER 0x1A // включение/выключение подсветки кнопки Холодная вода
+#define SET_BACKLIGHT_BUTTON_VOSK       0x1B // включение/выключение подсветки кнопки Воск
+#define SET_BACKLIGHT_BUTTON_OSMOS      0x1C // включение/выключение подсветки кнопки Осмос
+#define SET_BACKLIGHT_BUTTON_STOP       0x1D // включение/выключение подсветки кнопки Стоп
+#define PUSH_BUTTON_INSECT              0x1E // Нажата кнопка Средство от насекомых
+#define PUSH_BUTTON_COLLECTION          0x1F // Нажата кнопка Инкассация
+#define SET_DISPLAY_NUMBER              0x20 // Установка значения на дисплее
+#define PUSH_JETTON_CHANNEL_1           0x21 // Принят жетон по первому каналу
+#define PUSH_JETTON_CHANNEL_2           0x22 // Принят жетон по 2 каналу
+#define PUSH_JETTON_CHANNEL_3           0x23 // Принят жетон по 3 каналу
+#define PUSH_BUTTON_FOAM                0x24 // Нажата кнопка Пена
+#define PUSH_BUTTON_FOAM_WATER          0x25 // Нажата кнопка Пена + вода
+#define PUSH_BUTTON_HOT_WATER           0x26 // Нажата кнопка Горячая вода
+#define PUSH_BUTTON_COOL_WATER          0x27 // Нажата кнопка Холодная вода
+#define PUSH_BUTTON_VOSK                0x28 // Нажата кнопка Воск
+#define PUSH_BUTTON_OSMOS               0x29 // Нажата кнопка Осмос
+#define PUSH_BUTTON_STOP                0x2A // Нажата кнопка Стоп
 
 #endif //PULT_GLOBALSETTINGS_H
